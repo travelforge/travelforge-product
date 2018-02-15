0ce04ea99805ddff109b8b371410c9eddb9347e9
Matthias Deck <md@matthiasdeck.de>
2018-02-15 15:16:38 +0100

    Update copyright header

--- travelforge-product-service/src/main/java/org/travelforge/product/service/RequestOptions.java
+++ travelforge-product-service/src/main/java/org/travelforge/product/service/RequestOptions.java
@@ -2,7 +2,7 @@
  * The Travelforge Project
  * http://www.travelforge.org
  *
- * Copyright (c) 2015 - 2017 Matthias Deck
+ * Copyright (c) 2015-present Matthias Deck
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
